28e2d0d1afcef69adb8a711b1f6db9e2c20dfc42
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2019-12-02 00:06:25 +0100

    Eq + bugfix

--- fibin.h
+++ fibin.h
@@ -1,11 +1,36 @@
 #include <iostream>
 
-template <int n>
-struct Fib : std::integral_constant<int, Fib<n- 1>::value + Fib<n - 2>::value> {};
+class AbstractLiteral {
+public:
+    virtual ~AbstractLiteral() = 0;
+};
+
+template <uint64_t n>
+class Fib : private AbstractLiteral, public std::integral_constant<uint64_t, Fib<n - 1>::value + Fib<n - 2>::value> {};
 
 template<>
-struct Fib<1> : std::integral_constant<int, 1> {};
+class Fib<1> : private AbstractLiteral, public std::integral_constant<uint64_t, 1> {};
 
 template<>
-struct Fib<0> : std::integral_constant<int, 0> {};
+class Fib<0> : private AbstractLiteral, public std::integral_constant<uint64_t, 0> {};
 
+class True : private AbstractLiteral, public std::true_type {};
+
+class False : private AbstractLiteral, public std::false_type {};
+
+template<typename T, typename = typename std::enable_if<std::is_base_of<AbstractLiteral, T>::value>::type>
+class Lit {};
+
+template<uint64_t n>
+class Lit<Fib<n>> : public std::integral_constant<uint64_t, Fib<n>::value> {};
+
+template<>
+class Lit<True> : public std::true_type {};
+
+template<>
+class Lit<False> : public std::false_type {};
+
+template<typename T, typename U>
+class Eq : public std::bool_constant<(
+        std::is_same<decltype(T::value), decltype(U::value)>::value &&
+        T::value == U::value)> {};
