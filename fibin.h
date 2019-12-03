b1d2fa3f55fc6a32a6c366c9c906c509eeba4be6
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2019-12-04 00:38:14 +0100

    fix

--- fibin.h
+++ fibin.h
@@ -1,36 +1,113 @@
 #include <iostream>
-
-class AbstractLiteral {
-public:
-    virtual ~AbstractLiteral() = 0;
-};
-
-template <uint64_t n>
-class Fib : private AbstractLiteral, public std::integral_constant<uint64_t, Fib<n - 1>::value + Fib<n - 2>::value> {};
-
-template<>
-class Fib<1> : private AbstractLiteral, public std::integral_constant<uint64_t, 1> {};
-
-template<>
-class Fib<0> : private AbstractLiteral, public std::integral_constant<uint64_t, 0> {};
-
-class True : private AbstractLiteral, public std::true_type {};
-
-class False : private AbstractLiteral, public std::false_type {};
-
-template<typename T, typename = typename std::enable_if<std::is_base_of<AbstractLiteral, T>::value>::type>
-class Lit {};
+#include <type_traits>
 
 template<uint64_t n>
-class Lit<Fib<n>> : public std::integral_constant<uint64_t, Fib<n>::value> {};
+struct Fib {};
+
+struct True : public std::true_type {};
+
+struct False : public std::false_type {};
+
+template<typename T>
+struct isFib : public std::false_type {};
+
+template<uint64_t n>
+struct isFib<Fib<n>> : public std::true_type {};
+
+template<typename T>
+struct isBoolean : public std::false_type {};
 
 template<>
-class Lit<True> : public std::true_type {};
+struct isBoolean<True> : public std::true_type {};
 
 template<>
-class Lit<False> : public std::false_type {};
+struct isBoolean<False> : public std::true_type {};
 
-template<typename T, typename U>
-class Eq : public std::bool_constant<(
-        std::is_same<decltype(T::value), decltype(U::value)>::value &&
-        T::value == U::value)> {};
+template<typename T, typename = typename std::enable_if<(isBoolean<T>::value || isFib<T>::value)>::type>
+struct Lit {};
+
+template<uint64_t n>
+struct Lit<Fib<n>> {};
+
+template<>
+struct Lit<True> {};
+
+template<>
+struct Lit<False> {};
+
+template<typename T>
+struct isLit : std::false_type {};
+
+template<>
+struct isLit<Lit<True>> : std::true_type {};
+
+template<>
+struct isLit<Lit<False>> : std::true_type {};
+
+template<uint64_t n>
+struct isLit<Lit<Fib<n>>> : std::true_type {};
+
+template<typename Condition, typename Then, typename Else>
+struct If {};
+
+template<typename Left, typename Right>
+struct Eq {};
+
+template<typename V, typename T>
+struct Eval {};
+
+template<typename V, uint64_t n>
+struct Eval<V, Fib<n>> : public std::integral_constant<V, Eval<V, Fib<n - 1>>::value + Eval<V, Fib<n - 2>>::value> {};
+
+template<typename V>
+struct Eval<V, Fib<1>> : public std::integral_constant<V, 1> {};
+
+template<typename V>
+struct Eval<V, Fib<0>> : public std::integral_constant<V, 0> {};
+
+//template<typename V>
+//struct Eval<V, Lit<True>> {
+//    using result = True;
+//};
+//
+//template<typename V>
+//struct Eval<V, Lit<False>> {
+//    using result = False;
+//};
+
+template<typename V, uint64_t n>
+struct Eval<V, Lit<Fib<n>>> {
+    using result = Eval<V, Fib<n>>;
+};
+
+template<typename V, typename Then, typename Else>
+struct Eval<V, If<Lit<True>, Then, Else>> {
+    using result = typename Eval<V, Then>::result;
+};
+
+template<typename V, typename Then, typename Else>
+struct Eval<V, If<Lit<False>, Then, Else>> {
+    using result = typename Eval<V, Else>::result;
+};
+
+template<typename V, typename Condition, typename Then, typename Else>
+struct Eval<V, If<Condition, Then, Else>> {
+    using result = typename Eval<V, If<typename Eval<V, Condition>::result, Then, Else>>::result;
+};
+
+template<typename ValueType>
+struct Fibin {
+    template<typename Expr, typename V = ValueType>
+    static constexpr typename std::enable_if<std::is_integral<V>::value, V>::type
+    eval() {
+        return Eval<V, Expr>::result::value;
+    }
+
+    template<typename Expr, typename V = ValueType>
+    static constexpr typename std::enable_if<!(std::is_integral<V>::value)>::type
+    eval() {
+        std::cout << "Fibin doesn't support: PKc" << std::endl;
+    }
+};
+
+
